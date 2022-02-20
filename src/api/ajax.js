bbfd223ca204e0e5ef89b7143e071726a477af42
Garrukzijian <u6128492@au.edu>
2022-02-21 01:47:26 +0700

    use the axios to make the login request

---
+++ src/api/ajax.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import qs from "qs"
+
+axios.interceptors.request.use(function(config){
+
+    const{method,data} = config
+    if(method.toLowerCase()==='post' && typeof data ==="object"){
+       config.data =  qs.stringify(data)
+    }
+    return config
+});
+
+axios.interceptors.response.use(function(response){
+    return response.data;
+},function(error){
+    alert('fail1'+error.message)
+    return new Promise(()=>{})
+});
+
+
+export default axios
